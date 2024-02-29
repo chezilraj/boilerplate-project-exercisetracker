2d4f96adf89e2fde28af83dd940df143264d0658
Chezil <chezilraj@Chezils-MacBook-Air.local>
2024-02-29 21:00:58 +0400

    bug fixes

--- index.js
+++ index.js
@@ -8,19 +8,21 @@ const shortid = require('shortid');
 let mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI);
 const Schema = mongoose.Schema;
+
 const userSchema = new Schema({
-  name: String,
-	_id: String
+	username: String,
 });
 
-const addExerciseSchema = new Schema({
-  _id: String,
-	description: String,
-	duration: Number,
-	date: String
+const exerciseSchema = new Schema({
+	userId: String,
+	username: String,
+	description: {type: String, required: true},
+	duration: {type: Number, required: true},
+	date: {type: Date, default: Date.now(), required: false},
 });
+
 const User = mongoose.model('User', userSchema);
-const AddExercise = mongoose.model('AddExercise', addExerciseSchema);
+const Exercise = mongoose.model('Exercise', exerciseSchema);
 
 app.use(cors())
 app.use(express.static('public'))
@@ -30,30 +32,25 @@ app.get('/', (req, res) => {
 
 
 app.post('/api/users', async(req, res) => {
-	console.log(req.body)
 	const username = req.body.username;
-	const userId = shortid.generate();
+
 	try {
-		await User.create({name : username, _id: userId})
-		res.json({username : username, _id: userId})
+		let newUser = await User.create({username})
+		res.json({username : username, _id: newUser._id })
 	}
 	catch (err) {
 		console.error(err);
 		res.status(500).json({ error: 'Internal Server Error' });
 	}
 })
-
-app.post('/api/users/:_id/exercises', async(req, res) => {
-	const userId = req.params._id;
-	const description = req.body.description;
-	const duration = req.body.duration;
-	const date = req.body.date;
-	console.log(description);
+app.get('/api/users', async(req, res) => {
 	try {
-			const user = await User.find({_id: userId});
-			if(user && user.length > 0){
-				await AddExercise.create({_id : userId, description: description, duration: duration, date: date})
-				res.json({_id : userId, username: user.name, date: date, duration: duration, description: description})
+		// await User.deleteMany({});
+    // await Exercise.deleteMany({});
+		// res.json({ message: 'All data removed from the database.' });
+			const users = await User.find();
+			if(users && users.length > 0){
+				res.json(users)
 			}else{
 				return res.status(404).json({ error: 'User not found' });
 			}
@@ -63,7 +60,72 @@ app.post('/api/users/:_id/exercises', async(req, res) => {
 		res.status(500).json({ error: 'Internal Server Error' });
 	}
 })
+app.post('/api/users/:_id/exercises', async(req, res) => {
+	try {
+			const userId = req.params._id;
+			const {description, duration, date} = req.body;
+			
 
+			const user = await User.findById(userId);
+			if (!user) {
+				return res.status(404).json({ error: 'User not found' });
+			}
+			const exercise = new Exercise({ 
+				userId: user._id,
+				username: user.username,
+				description: description,
+				duration: parseInt(duration),
+				date: date,
+			 });
+			let saveExercise = await exercise.save();
+			
+			res.json({
+				username: saveExercise.username,
+				description: exercise.description,
+				duration: exercise.duration,
+				date: new Date(exercise.date).toDateString(),
+				_id: saveExercise._id,
+			});
+	}
+	catch (err) {
+		console.error(err);
+		res.status(500).json({ error: 'Internal Server Error' });
+	}
+})
+app.get('/api/users/:_id/logs', async(req, res) => {
+	try {
+			const userId = req.params._id;
+			const from = req.query.from || new Date(0).toISOString().substring(0, 10);
+			const to =
+				req.query.to || new Date(Date.now()).toISOString().substring(0, 10);
+			const limit = parseInt(req.query.limit) || null;
+
+			const user = await User.findById(userId).exec();
+
+			const exercises = await Exercise.find({userId, date: { $gte: from, $lte: to }})
+			.select('description duration date')
+			.limit(limit)
+			.exec();
+
+			let parsedDatesLog = exercises.map((exercise) => {
+				return {
+					description: exercise.description,
+					duration: exercise.duration,
+					date: new Date(exercise.date).toDateString(),
+				};
+			});
+			res.json({
+				_id: user._id,
+				username: user.username,
+				count: parsedDatesLog.length,
+				log: parsedDatesLog,
+			});
+	}
+	catch (err) {
+		console.error(err);
+		res.status(500).json({ error: 'Internal Server Error' });
+	}
+})
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
